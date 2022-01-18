9f3c23c6da4cd3ddac2b6ab8b42ddd5e270367ee
eireren <irene9ago85@hotmail.com>
2022-01-18 20:16:48 +0100

    project-continue

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
 url1 = "https://datos.madrid.es/egob/catalogo/202318-0-escuelas-infantiles.json"
 url2 = "https://datos.madrid.es/egob/catalogo/202311-0-colegios-publicos.json"  
 path = "../data/dbo.bicimad_stations.csv"
+path1 = "../data/example2.csv"
+
 
 
 #main code
@@ -36,3 +38,11 @@ bicimad_cleaned_df = wr.split_lat_long_and_cleaning(bicimad_df)
 an.add_key_column(places_of_interest_df)
 an.add_key_column(bicimad_cleaned_df)
 an.apply_mercator_to_df(bicimad_cleaned_df)
+an.apply_mercator_to_df(places_of_interest_df)
+merged_df = an.merging_by_column (places_of_interest_df,bicimad_cleaned_df,"key")
+total_df = an.apply_distance_to_df(merged_df)
+
+#reporting
+rp.save_data_to_csv(final_df_complete_version,path1)
+project1_fdf = pd.DataFrame(columns = ["Place of interest","Place address","BiciMAD station","Station location","Distance"])
+project1_fdf[["Place of interest","Place address","BiciMAD station","Station location","Distance"]] = final_df[["title","address.street-address","name","address","distance"]]
