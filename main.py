fdc71eac653391a72880a2971227640a19a26379
eireren <irene9ago85@hotmail.com>
2022-01-18 22:03:54 +0100

    project-continue

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from p_analysis import analysis as an
 from p_reporting import reporting as rp
 pd.set_option('display.max_rows', 309)
 pd.set_option('display.max_columns', 7)
-pd.set_option('display.width', 1000)
 
 #Variables
 url1 = "https://datos.madrid.es/egob/catalogo/202318-0-escuelas-infantiles.json"
